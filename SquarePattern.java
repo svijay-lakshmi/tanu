851f66e45e63cbad530ebd0ba7dccfea5dba7cd4
svijay-lakshmi <147981664+svijay-lakshmi@users.noreply.github.com>
2024-05-28 12:05:08 +0530

    Add files via upload

---
+++ SquarePattern.java
@@ -0,0 +1,12 @@
+public class SquarePattern {
+    public static void main(String[] args) {
+        int size = 5; // Change this value to adjust the size of the square
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
